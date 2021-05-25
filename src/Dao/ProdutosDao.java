eb709f1d93035734c09665701902937c0a10179a
gesse-fernandes <gesse-fm@hotmail.com>
2021-05-24 21:57:25 -0300

    conclusao no inserir dao agora controller

--- src/Dao/ProdutosDao.java
+++ src/Dao/ProdutosDao.java
@@ -6,13 +6,36 @@ import java.sql.ResultSet;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
+import Model.ProdutosModel;
 import Util.Conexao;
+import Util.Corretores;
 
 public class ProdutosDao {
     
     public ProdutosDao()
     {
 
+    }
+
+    public void inserir(ProdutosModel produtosModel)
+    {
+        try {
+            String INSERT = "INSERT INTO produtos(nome,data_validade,qtd,valorUnitario,valorTotal)VALUES(?,?,?,?,?)";
+            PreparedStatement st = Conexao.getConnection().prepareStatement(INSERT);
+            st.setString(1, produtosModel.getNome());
+            st.setString(2, Corretores.ConverterParaSQL(produtosModel.getDataValidade()));
+            st.setInt(3, produtosModel.getQtd());
+            st.setDouble(4, produtosModel.getValorUnitario());
+            st.setDouble(5, produtosModel.getValorTotal());
+            st.execute();
+            Conexao.getConnection().commit();
+            JOptionPane.showMessageDialog(null, "Registro salvo com sucesso", "ok", 1, new ImageIcon(
+                    "C:\\Users\\gesse\\OneDrive\\Documentos\\NetBeansProjects\\ProjetoCrudQ2\\src\\Imagens\\ok.png"));
+        } catch (Exception e) {
+            //TODO: handle exception
+            JOptionPane.showMessageDialog(null, "erro ao cadastrar", "Erro", 0, new ImageIcon(
+                    "C:\\Users\\gesse\\OneDrive\\Documentos\\NetBeansProjects\\ProjetoCrudQ2\\src\\Imagens\\btn_sair.png"));
+        }
     }
 
     public String ProximoProduto() {
